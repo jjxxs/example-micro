39b5c752a278b53c021347f8cc7131896185df9e
Oliver Braun <ob@cs.hm.edu>
2020-05-16 11:20:15 +0200

    refactor logger -> logwriter

---
+++ logwriter/logger.go
@@ -0,0 +1,40 @@
+package logwriter
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/store"
+	"github.com/vesose/example-micro/api"
+)
+
+type LogWriter struct {
+	store store.Store
+}
+
+func New(store store.Store) *LogWriter {
+	return &LogWriter{store: store}
+}
+
+func (s *LogWriter) Process(ctx context.Context, event *api.Event) error {
+	md, _ := metadata.FromContext(ctx)
+	logger.Infof("Received event %+v with metadata %+v", event, md)
+
+	newSleep := []byte(fmt.Sprintf("%v", rand.Intn(2000)))
+
+	record := store.Record{
+		Key:    "sleep",
+		Value:  newSleep,
+		Expiry: 0,
+	}
+	err := s.store.Write(&record, func(o *store.WriteOptions) { o.Table = "sleeper" })
+
+	if err != nil {
+		logger.Infof("error while writing to store: %+v", err)
+	}
+
+	return err
+}
--- logwriter/service/main.go
+++ logwriter/service/main.go
@@ -1,48 +1,20 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"strings"
 
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/logger"
-	"github.com/micro/go-micro/v2/metadata"
-	"github.com/micro/go-micro/v2/store"
 	nats "github.com/micro/go-plugins/broker/nats/v2"
 	zl "github.com/micro/go-plugins/logger/zerolog/v2"
 	"github.com/micro/go-plugins/registry/etcdv3/v2"
 	"github.com/micro/go-plugins/store/redis/v2"
 	"github.com/rs/zerolog"
-	"github.com/vesose/example-micro/api"
+	"github.com/vesose/example-micro/logwriter"
 )
-
-type Sub struct {
-	store store.Store
-}
-
-func (s *Sub) Process(ctx context.Context, event *api.Event) error {
-	md, _ := metadata.FromContext(ctx)
-	logger.Infof("Received event %+v with metadata %+v", event, md)
-
-	newSleep := []byte(fmt.Sprintf("%v", rand.Intn(2000)))
-
-	record := store.Record{
-		Key:    "sleep",
-		Value:  newSleep,
-		Expiry: 0,
-	}
-	err := s.store.Write(&record, func(o *store.WriteOptions) { o.Table = "sleeper" })
-
-	if err != nil {
-		logger.Infof("error while writing to store: %+v", err)
-	}
-
-	return err
-}
 
 func main() {
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
@@ -58,7 +30,7 @@ func main() {
 	store := redis.NewStore()
 
 	service := micro.NewService(
-		micro.Name("logWriter"),
+		micro.Name("logwriter"),
 		micro.Version("latest"),
 		micro.Registry(registry),
 		micro.Broker(broker),
@@ -68,9 +40,7 @@ func main() {
 	service.Init()
 
 	if err := micro.RegisterSubscriber("log.*", service.Server(),
-		&Sub{
-			store: store,
-		}); err != nil {
+		logwriter.New(store)); err != nil {
 		panic(err)
 	}
 
